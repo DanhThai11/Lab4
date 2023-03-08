9bcd26948573b995e177d77bec77e9a791e51bd5
DanhThai11 <125196318+DanhThai11@users.noreply.github.com>
2023-03-08 20:49:39 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,10 @@
+package lab4;
+import lab4.DanhSachSinhVien;
+public class test {
+    public static void main (String[] arg)
+    {
+    DanhSachSinhVien dssv = new DanhSachSinhVien();
+    dssv.menu();
+    
+    }
+}
